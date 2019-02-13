c5f5ef7c767380671964e4aad219e1785f0a96bc
Prince Ugwuh <prince.ugwuh@gmail.com>
2019-02-13 13:08:06 -0600

    open/public scope updates

--- SwiftAdditions/Sources/Common/Currency.swift
+++ SwiftAdditions/Sources/Common/Currency.swift
@@ -1,12 +1,12 @@
 import Foundation
 import CoreGraphics
 
-protocol Currency {
+public protocol Currency {
     var decimalValue: Decimal { get } // maintain precision
 }
 
 extension Currency {
-    func currency(_ fractDigits: Int = 0, locale: Locale = .current) -> String {
+    public func currency(_ fractDigits: Int = 0, locale: Locale = .current) -> String {
         let formatter = NumberFormatter()
         formatter.numberStyle = .currency
         formatter.locale = locale
@@ -17,25 +17,25 @@ extension Currency {
 }
 
 extension Double: Currency {
-    var decimalValue: Decimal {
+    public var decimalValue: Decimal {
         return Decimal(self)
     }
 }
 
 extension Int: Currency {
-    var decimalValue: Decimal {
+    public var decimalValue: Decimal {
         return Decimal(self)
     }
 }
 
 extension String: Currency {
-    var decimalValue: Decimal {
+    public var decimalValue: Decimal {
         return Decimal(string: self) ?? 0
     }
 }
 
 extension CGFloat: Currency {
-    var decimalValue: Decimal {
+    public var decimalValue: Decimal {
         return Decimal(Double(self))
     }
 }
